ffa91c80ec2020c226b48cc27f5c8c0837ac81e9
Shusuke Sato <sato.fun0619@gmail.com>
2019-11-26 15:19:32 +0900

    add hello world project file

---
+++ HelloSwiftUI/HelloSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HelloSwiftUI
+//
+//  Created by 佐藤秀輔 on 2019/11/26.
+//  Copyright © 2019 佐藤秀輔. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
