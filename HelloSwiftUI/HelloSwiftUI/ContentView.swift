6fb73e085f75d827f44e006ee188e10c36094558
Shusuke Sato <sato.fun0619@gmail.com>
2020-03-09 16:51:29 +0900

    add button

--- HelloSwiftUI/HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/HelloSwiftUI/ContentView.swift
@@ -9,13 +9,28 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var labelText = "Hello World!"
+    
     var body: some View {
         // 縦方向にまとめるUI
         VStack(alignment: .center) {
-            Text("Hello, World!")
+            Text(labelText)
                 .font(.title)
                 .padding()
             Text("This space is vertical stack")
+            
+            // Label Text change button
+            Button(action: {
+                self.labelText = "Hello SwiftUI"
+            }) {
+                Text("Tap!")
+                    .font(.footnote)
+                    .fontWeight(.thin)
+                    .foregroundColor(.black)
+                    .frame(width: 70, height: 20)
+                    .border(Color.gray)
+            }.padding()
+            
             // 横方向にまとめるUI
             HStack {
                 Text("Practice SwiftUI Project")
