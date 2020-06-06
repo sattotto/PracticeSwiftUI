7e04c038e5cd3a77e10ac95a0b397a1937469805
Shusuke Sato <sato.fun0619@gmail.com>
2020-06-06 15:34:17 +0900

    入力Form作成

--- FormSample/FormSample/ContentView.swift
+++ FormSample/FormSample/ContentView.swift
@@ -18,17 +18,22 @@ struct ContentView: View {
     var body: some View {
         NavigationView {
             // List, Formで表示が違う
-            // Listのほうがよりコンパクトで表示が見やすい
+            // Formのほうがよりコンパクトで表示が見やすい
             Form {
-                TextField("プレイヤー名を入力してください", text: $name)
-                Text("こんにちは，\(name)!")
-                DatePicker(selection: $playDate,
-                          label: {Text("年月日")} )
-                Picker(selection: $selected,
-                       label: Text("ゲーム名を選択"))
-                {
-                    ForEach(0..<game.count) {
-                        Text(self.game[$0])
+                // section でくくると入力場所を見やすくできる
+                Section(header: Text("Player情報入力")) {
+                    TextField("プレイヤー名を入力してください", text: $name)
+                    Text("こんにちは，\(name)")
+                    DatePicker(selection: $playDate,
+                    label: {Text("年月日")} )
+                }
+                Section(header: Text("ゲーム選択")) {
+                    Picker(selection: $selected,
+                           label: Text("ゲーム名を選択"))
+                    {
+                        ForEach(0..<game.count) {
+                            Text(self.game[$0])
+                        }
                     }
                 }
                 Button(action: {}) {
