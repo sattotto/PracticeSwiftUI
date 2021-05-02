334485ccd3a4e92f076588f1bc0f05dfe1433be0
ShusukeSato <sato.fun0619@gmail.com>
2021-05-02 21:22:02 +0900

    add tabbar swift ui sample

---
+++ TABBarSample/Shared/TABBarSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  TABBarSampleApp.swift
+//  Shared
+//
+//  Created by 佐藤秀輔 on 2021/05/02.
+//
+
+import SwiftUI
+
+@main
+struct TABBarSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
