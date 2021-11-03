1014f34637747929e96293b366368d7e63e7e1df
Shusuke Sato <sato.fun0619@gmail.com>
2021-11-03 13:57:48 +0900

    add HostingViewController Sample

---
+++ HostingViewControllerSample/HostingViewControllerSample/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HostingViewControllerSample
+//
+//  Created by 佐藤秀輔 on 2021/11/03.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ HostingViewControllerSample/HostingViewControllerSample/HostingViewControllerSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  HostingViewControllerSampleApp.swift
+//  HostingViewControllerSample
+//
+//  Created by 佐藤秀輔 on 2021/11/03.
+//
+
+import SwiftUI
+
+@main
+struct HostingViewControllerSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
