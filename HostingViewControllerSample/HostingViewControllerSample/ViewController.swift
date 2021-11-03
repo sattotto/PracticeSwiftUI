30bc5476dff30e391a3c87bafa5dda11025087a2
Shusuke Sato <sato.fun0619@gmail.com>
2021-11-03 14:09:55 +0900

    change project setting

---
+++ HostingViewControllerSample/HostingViewControllerSample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HostingViewControllerSample
+//
+//  Created by 佐藤秀輔 on 2021/11/03.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
